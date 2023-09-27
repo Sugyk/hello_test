443b861aaaada6b61a1a03c02ea733aac11e4c20
Oleg Zaharov <active.sugyk@gmail.com>
2023-09-27 15:51:29 +0300

    result

---
+++ test_hello/say_hello/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    name = request.GET.get('name')
+    message = request.GET.get('message')    
+    return render(request, 'index.html', {'name': name, 'message': message})
